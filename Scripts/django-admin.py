1c09504b9dcaccf82943e9970502cb2fec1a340a
MomoNaman <namankeita004@gmail.com>
2022-01-16 20:09:43 +0100

    My Django Girls app, first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\naman\Desktop\Master2 1er semestre\Dev Informatique\Djangogirls\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
