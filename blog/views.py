ecf0a1ca56034beaeb6afa59dc306379a81e644a
MomoNaman <namankeita004@gmail.com>
2022-02-01 16:34:33 +0100

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/views.py
+++ blog/views.py
@@ -5,3 +5,6 @@ def post_list(request):
     Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
 # Create your views here.
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
